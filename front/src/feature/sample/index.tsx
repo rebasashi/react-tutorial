7cee2a105ea62f15ebc07533b15566fcaad90a31
rebasashi <shilva@hotmail.co.jp>
2021-11-02 14:32:52 +0000

    update: react-main-02 JSXに式を埋め込む

--- front/src/feature/sample/index.tsx
+++ front/src/feature/sample/index.tsx
@@ -5,9 +5,14 @@ export const Sample: React.FC = () => {
   // JSX式の導入
   const element = <h1>Hello, world!</h1>
 
+  // JSXに式を埋め込む
+  const name = 'Josh Perez'
+  const element2 = <h2>Hello, {name}</h2>
+
   return (
     <div>
       {element}
+      {element2}
     </div>
   )
 }
