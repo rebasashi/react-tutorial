d87bec391dc6d12eee8b5c645c374e778fcae6a4
rebasashi <shilva@hotmail.co.jp>
2021-11-02 14:29:07 +0000

    update: react-main-02 JSXの導入

---
+++ front/src/feature/sample/index.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
+
+export const Sample: React.FC = () => {
+  // JSX式の導入
+  const element = <h1>Hello, world!</h1>
+
+  return (
+    <div>
+      {element}
+    </div>
+  )
+}
