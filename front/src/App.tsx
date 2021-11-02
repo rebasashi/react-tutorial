dd49b046b1ad948f0a32120575412df04b75249b
rebasashi <shilva@hotmail.co.jp>
2021-11-02 13:49:13 +0000

    refactor: allow function

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App () {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
