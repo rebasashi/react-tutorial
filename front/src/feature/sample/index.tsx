ee16412223533fc0e1cdcfc815066fc4c8fdc0c0
rebasashi <shilva@hotmail.co.jp>
2021-11-02 15:20:16 +0000

    update: react-main-06 init

--- front/src/feature/sample/index.tsx
+++ front/src/feature/sample/index.tsx
@@ -1,18 +1,10 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react'
 
-// コンポーネント名は大文字で始める
-const Welcome = (props: { name: string }) => {
-  return <h1>Hello, {props.name}</h1>
-}
-
 export const Sample: React.FC = () => {
-  // コンポーネントを組み合わせる
   return (
     <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
+      Hoge
     </div>
   )
 }
