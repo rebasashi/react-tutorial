0e9a5e621da6de695976818077cefad312ac8b6d
rebasashi <shilva@hotmail.co.jp>
2021-11-02 15:28:26 +0000

    update: material-ui 表示確認

--- front/src/feature/sample/index.tsx
+++ front/src/feature/sample/index.tsx
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react'
+import { Button } from '@material-ui/core'
 
 export const Sample: React.FC = () => {
   return (
     <div>
-      Hoge
+      <Button color="primary">Hello World</Button>
     </div>
   )
 }
