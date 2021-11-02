03710ab0a011da1519c844e222f9ce3d594526f2
rebasashi <shilva@hotmail.co.jp>
2021-11-02 14:42:41 +0000

    update: react-main-02 JSX式にTypeScript関数の結果を埋め込む

--- front/src/feature/sample/index.tsx
+++ front/src/feature/sample/index.tsx
@@ -2,17 +2,20 @@
 import React from 'react'
 
 export const Sample: React.FC = () => {
-  // JSX式の導入
-  const element = <h1>Hello, world!</h1>
+  const formatName = (user: { firstName: string; lastName: string }) => {
+    return user.firstName + ' ' + user.lastName
+  }
+  const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+  }
 
-  // JSXに式を埋め込む
-  const name = 'Josh Perez'
-  const element2 = <h2>Hello, {name}</h2>
+  // JSX式にTypeScript関数の結果を埋め込む
+  const element = <h1>Hello, {formatName(user)}</h1>
 
   return (
     <div>
       {element}
-      {element2}
     </div>
   )
 }
