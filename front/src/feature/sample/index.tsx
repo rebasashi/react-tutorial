3774d09abd71e86afdbeb12fe3559cfa206ced8a
rebasashi <shilva@hotmail.co.jp>
2021-11-02 15:38:37 +0000

    update: react-main-06 イベントハンドラ

--- front/src/feature/sample/index.tsx
+++ front/src/feature/sample/index.tsx
@@ -1,11 +1,19 @@
 // eslint-disable-next-line no-use-before-define
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 import { Button } from '@material-ui/core'
+
+// イベントハンドラ関数
+const buttonHandler = (e: SyntheticEvent) => {
+  // Reactでイベントハンドラ引数を用いる場合SyntheticEventインスタンスが渡される
+  // 参考：https://ja.reactjs.org/docs/events.html
+  // 不要であれば引数eを付けなくても良いと思われる
+  console.log(e)
+}
 
 export const Sample: React.FC = () => {
   return (
     <div>
-      <Button color="primary">Hello World</Button>
+      <Button color="primary" onClick={buttonHandler}>Hello World</Button>
     </div>
   )
 }
