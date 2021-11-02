47d87e613ab5f69f8b08509ce35e1902ad69bc27
rebasashi <shilva@hotmail.co.jp>
2021-11-02 15:09:57 +0000

    update: react-main-04 コンポーネントのレンダー

--- front/src/feature/sample/index.tsx
+++ front/src/feature/sample/index.tsx
@@ -1,10 +1,18 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react'
 
+// コンポーネント名は大文字で始める
+const Welcome = (props: { name: string }) => {
+  return <h1>Hello, {props.name}</h1>
+}
+
+// 小文字で始まるコンポーネントはDOMタグとして扱われる
+const element = <Welcome name="Sara" />
+
 export const Sample: React.FC = () => {
   return (
     <div>
-      hoge
+      {element}
     </div>
   )
 }
